d4faa13f5834a387b6435d7131f11f15b864c97c
Vicente <vjrodrig@gmail.com>
2021-12-06 08:04:09 -0300

    Component-compound listo

---
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,37 @@
+import styles from '../styles/styles.module.css'
+import { useProduct } from '../hooks/useProduct';
+import { createContext } from 'react';
+import { ProductCardProps, ProductContextProps } from '../interfaces/interfaces';
+import { ProductTitle } from './ProductTitle';
+import { ProductImage } from './ProductImage';
+import { ProductButtons } from './ProductButtons';
+
+
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+export const ProductCard = ({ children, product }: ProductCardProps) => {
+
+    const { counter, increaseBy } = useProduct();
+
+    return (
+        <Provider value={{
+            counter,
+            increaseBy,
+            product
+        }}>
+            <div className={ styles.productCard }>
+                { children }
+                {/* <ProductImage img={ product.img }/>
+                <ProductTitle title={ product.title }/>
+                <ProductButtons increaseBy={ increaseBy } counter={ counter }/> */}
+                   
+            </div>
+        </Provider>
+        
+    )
+}
+
+ProductCard.Title = ProductTitle;
+ProductCard.Image = ProductImage;
+ProductCard.Buttons = ProductButtons;
---
+++ src/02-component-patterns/components/index.ts
@@ -0,0 +1,17 @@
+import { ProductCard as ProductCardHOC } from './ProductCard';
+import { ProductButtons } from './ProductButtons';
+import { ProductTitle } from './ProductTitle';
+import { ProductImage } from './ProductImage';
+
+
+export { ProductButtons } from './ProductButtons';
+export { ProductImage } from './ProductImage';
+export { ProductTitle } from './ProductTitle';
+
+export const ProductCard = Object.assign( ProductCardHOC, {
+    Title: ProductTitle,
+    Image: ProductImage,
+    Buttons: ProductButtons
+
+});
+
